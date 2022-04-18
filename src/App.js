fe567713f11b02df2dbb76361769a3ac01423080
Monisha Dolai <monishadolai1998@gmail.com>
2022-04-18 21:47:05 +0530

    first react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to React Monisha, Now the problem is resolved.
         </p>
         <a
           className="App-link"
